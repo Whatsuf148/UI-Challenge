c1bfc022a35a016420c2477fb5962414968c1cc3
Whatsuf148 <whatsufsachin@gmail.com>
2023-01-04 19:34:42 +0545

    Profile Page Design Creation

--- lib/main.dart
+++ lib/main.dart
@@ -315,51 +315,235 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Image.asset(
-          "assets/rec.jpg",
-          height: 400,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-        // ignore: prefer_const_literals_to_create_immutables
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/rec.jpg",
+            height: 220,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
           // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            Padding(
-                padding: EdgeInsets.only(top: 30, left: 17),
-                child: Icon(Icons.arrow_back_ios)),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Icon(Icons.arrow_back_ios),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30, left: 256),
-              child: Icon(Icons.message_rounded),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            SizedBox(
-              width: 600,
-              height: 300,
-            ),
-            Container(
-              height: 400,
-              width: 400,
-              //color: Colors.blue,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.blue,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: 30, left: 17),
+                  child: Icon(Icons.arrow_back_ios)),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Icon(Icons.arrow_back_ios),
               ),
-            ),
-          ],
-        )
-      ],
-    ));
+              Padding(
+                padding: const EdgeInsets.only(top: 30, left: 256),
+                child: Icon(Icons.message_rounded),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: 600,
+                height: 175,
+              ),
+              Container(
+                height: 410,
+                width: 400,
+                //color: Colors.blue,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              SizedBox(
+                width: 1,
+                height: 405,
+              ),
+              Text(
+                """1K 
+            Followers""",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  right: 1,
+                ),
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                """ 223 
+            Following""",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              SizedBox(
+                width: 1,
+                height: 650,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5, left: 80),
+                child: Text("""My name is seven. I like UI and UX design
+                 and web development"""),
+              ),
+            ],
+          ),
+          // ignore: prefer_const_literals_to_create_immutables
+          Row(
+            children: [
+              SizedBox(
+                width: 1,
+                height: 765,
+              ),
+              
+                 Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: (){},
+                    child: Container(
+                      height: 40,
+                      width: 125,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.black,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          "Follow",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              
+               Padding(
+                  padding: const EdgeInsets.only(left: 99),
+                  child: InkWell(
+                    onTap: (){},
+                    child: Container(
+                      height: 40,
+                      width: 125,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          "Message",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              
+            ],
+          ),
+
+          Column(
+            children: [
+              SizedBox(
+                height: 450,
+                width: 1,
+              ),
+              // ignore: prefer_const_literals_to_create_immutables
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "All",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Photos",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 45),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Videos",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 460,
+                width: 1,
+              ),
+              Container(
+                height: 300,
+                width: double.infinity,
+                //color: Colors.blue,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.white,
+                ),
+                
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
